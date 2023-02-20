52729332486ed6d4de28fbb24ef0d6ba814be51d
IE06 <ie06@Amans-iMac.local>
2023-02-20 16:57:33 +0530

    learning git & github

---
+++ Git&githubDemo/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Git&githubDemo
+//
+//  Created by IE06 on 20/02/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        print("git & github learning")
+        print("add to staging area")
+        print("all files add staging area")
+        print("git status")
+    
+    }
+
+
+}
+
